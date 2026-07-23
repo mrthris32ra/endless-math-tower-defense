506599730583582ad428edff90cfddae2d1ed4fd
Mr. Thristan Ezra <250956140+mrthris32ra@users.noreply.github.com>
2026-07-23 18:59:38 +0700

    Update console-log.js

--- console-log.js
+++ console-log.js
@@ -1,2 +1,2 @@
 console.log("Inserting Complete. Booting up admin control panel...");
-
+console.log("Warning: Do not override the script system which can cause the game to crash and it may impact your device.")
